f635c0f346b6568a255b8c58d574d89cd8dc32eb
cevadev <carlos.villanueva.altuna@gmail.com>
2022-09-14 13:46:20 -0300

    Vanilla JS - SPA project

---
+++ ejercicios/spa/app/App.js
@@ -0,0 +1,3 @@
+/** Componente padre de toda la SPA
+ *  Llamado de componentes para dibujar en pantalla la interface de usuario
+ */
---
+++ ejercicios/spa/app/index.js
@@ -0,0 +1,5 @@
+/** Archivo principal que desencadena toda la programacion de la SPA (no es un componente)
+ *  Desencadena la invocacion hacia los demas componentes de la App
+ *  Manejo de la delegacion de eventos
+ *
+ */
