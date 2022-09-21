60e3a2300c8ea26eccd2462ca93c0ea9c1eb5408
cevadev <carlos.villanueva.altuna@gmail.com>
2022-09-21 14:40:01 -0300

    Single Page Application with Vanilla JS

--- ejercicios/spa/app/App.js
+++ ejercicios/spa/app/App.js
@@ -1,8 +1,13 @@
 /** Componente padre de toda la SPA
  *  Llamado de componentes para dibujar en pantalla la interface de usuario
  */
+
+import api from "./helpers/wp_api.js";
+
 export function App() {
   document.getElementById(
     "root"
   ).innerHTML = `<h1>Bienvenidos a mi primer SPA con VanilaJS</h1>`;
+
+  console.info(api);
 }
---
+++ ejercicios/spa/app/helpers/wp_api.js
@@ -0,0 +1,27 @@
+/**
+ * aqui almacenamos todas las variables que nos permiten consultar hacia un dominio de wordpress
+ * documentacion: https://developer.wordpress.org/rest-api/
+ */
+
+const NAME = "malvestida";
+const DOMAIN = `https://${NAME}.com`;
+// variable que hace referenia a la url de la wordpress api para obtener info del sito en particular
+const SITE = `${DOMAIN}/wp-json`;
+// variable que accede a toda la info que ofrece la api de wordpress
+const API_WP = `${SITE}/wp/v2`;
+// variable que apunta a todos los post. con _embed trae embebido toda la info referente al post
+const POSTS = `${API_WP}/posts?_embed`;
+// variable que apunta a la informacion de un post en particular
+const POST = `${API_WP}/posts`;
+// consulta de busqueda
+const SEARCH = `${API_WP}/search?_embed&search=`;
+
+export default {
+  name: NAME,
+  domain: DOMAIN,
+  site: SITE,
+  api_wp: API_WP,
+  posts: POSTS,
+  post: POST,
+  search: SEARCH,
+};
